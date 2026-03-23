c6bb3c75a5eeb68209fc12bf9e4dad6fbf8a3a2d
nabelis-jasi <nabelisj65@gmail.com>
2026-03-23 11:14:12 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -266,7 +266,7 @@ export default function Login({ selectedRole, onLoginSuccess, onBack }) {
   return (
     <div style={styles.wrapper}>
       <img 
-        src="/src/assets/Untitled design_20260319_161147_0000.png" 
+        src="/src/assets/logo.png" 
         alt="" 
         style={styles.backgroundLogo}
       />
