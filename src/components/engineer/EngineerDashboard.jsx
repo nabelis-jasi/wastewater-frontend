88314953bc3622808dcccbeecbf324d707d5c54a
nabelis-jasi <nabelisj65@gmail.com>
2026-03-24 07:51:00 +0200

    Update EngineerDashboard.jsx

--- src/components/engineer/EngineerDashboard.jsx
+++ src/components/engineer/EngineerDashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import MapView from './MapView';  // MapView now lives alongside dashboard components
 import DataEditor from './DataEditor';
 import ShapefileUploader from './ShapefileUploader';
 import DataSync from './DataSync';
